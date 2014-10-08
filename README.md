b6dc7eb6af88ca7530f37a2e0bc1bf32d3013c44
Keith Ball <keithball@keiths-mpb.brainloop.com>
2014-10-08 09:28:10 +0200

    initial implementation

---
+++ README.md
@@ -0,0 +1,22 @@
+Simple Fileserver 
+=======
+
+###A simple HTTP server to serve local files
+
+---
+
+##Features
+
+- Share a local path using HTTP
+
+---
+
+##Usage
+
+simplefilesystem -port=1979 -path=/home/username/documents  
+ 
+--
+
+##TODO:
+
+- Authentication
---
+++ simplefileserver.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os/user"
+)
+
+func main() {
+
+	// Get port
+	port := flag.String("port", "1979", "Port number to listen on")
+
+	// Work out the current users home directory
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get path
+	path := flag.String("path", usr.HomeDir, "Path to publish")
+
+	flag.Parse()
+
+	url := "localhost:" + *port
+	fmt.Printf("Publishing \"%v\" on \"%v\"", *path, url)
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(*path))))
+	if err := http.ListenAndServe(url, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
